94eaff8d8cacb02d06f762f8480fd01f9a7f411d
Owner-maker <lisitsyn.artem.d@gmail.com>
2022-03-28 02:28:58 +0400

    Add Queue-application project files

---
+++ queue-application/src/test/java/queueapplication/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package queueapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
